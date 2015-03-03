840eb98b63e1f15ad19fe8044ca46e090789b75c
Jingrong <JingRong@MacBook-Air.local>
2015-03-03 23:08:12 +0800

    Additional skeleton files

---
+++ A0000000X/search.py
@@ -0,0 +1,11 @@
+import re
+import nltk
+import sys
+import getopt
+
+# Python script for indexing
+
+"""
+In order to collect the vocabulary, you need to apply tokenization and stemming on the document text. You should use the NLTK tokenizers (nltk.sent_tokenize() and nltk.word_tokenize()) to tokenize sentences and words, and the NLTK Porter stemmer (class nlkt.stem.porter) to do stemming. You need to do case-folding to reduce all words to lower case.
+"""
+
