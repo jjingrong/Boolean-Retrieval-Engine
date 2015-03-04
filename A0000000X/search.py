bd2d4a852da7f070ef98ea72cc128613a8622fc1
Jingrong <JingRong@MacBook-Air.local>
2015-03-04 16:40:15 +0800

    Added skip pointers

--- A0000000X/search.py
+++ A0000000X/search.py
@@ -2,19 +2,20 @@ import re
 import nltk
 import sys
 import getopt
+import math
 from collections import deque
 
 # Python script for queries
 
 def performQueries(allQueries, dictionaryFile, postingsFile, outputFile):
-    
+
     # Define operator tuples (operator, precedence)
     opLeftBracket = ('(', 4)
     opRightBracket = (')', 4)
     opNot = ('NOT', 3)
     opAnd = ('AND', 2)
     opOr = ('OR', 1)
-    
+
     # Open queries file and do them sequentially
     # Content now stores each line
     with open(allQueries) as fileObj:
@@ -23,7 +24,7 @@ def performQueries(allQueries, dictionaryFile, postingsFile, outputFile):
     for eachLine in content:
         outputQ = deque()
         opStack = []
-        
+
         # Process query into the output stack and process from there
         for eachWord in eachLine:
             if eachWord != '(' or ')' or 'NOT' or 'AND' or 'OR':
@@ -39,26 +40,26 @@ def performQueries(allQueries, dictionaryFile, postingsFile, outputFile):
                     elif eachWord == opOr[0]:
                         if opOr[1] < opStack.peek()[1]:
                             outputQ.append(opStack.pop())
-                
+
                 if eachWord == opNot[0]:
                     opStack.append(opNot)
                 elif eachWord == opAnd[0]:
                     opStack.append(opAnd)
                 elif eachWord == opOr[0]:
                     opStack.append(opOr)
-                    
+
             elif eachWord == '(':
                 opStack.append(opLeftBracket)
             elif eachWord == ')':
                 while opStack.peek() != '(':
                     outputQ.append(opStack.pop())
                 opStack.pop()
-                
+
         while len(opStack) != 0:
-            outputQ.append(opStack.pop())  
-                  
+            outputQ.append(opStack.pop())
+
         # Process the query
-        
+
         # Do each query
         # to do - Query precedence
 
@@ -68,15 +69,26 @@ def merge(list1, list2):
     resultList = []
     i = j = 0
     # to do - Skip pointers
+    iSkipPointer = math.sqrt(len(list1))
+    iSkipPointer = math.floor(iSkipPointer)
+    jSkipPointer = math.sqrt(len(list2))
+    jSkipPointer = math.floor(jSkipPointer)
+
     while (i < len(list1) and j < len(list2)):
         if list1[i] == list2[j]:
             resultList.append(list1[i])
             i = i + 1
             j = j + 1
         elif list1[i] < list2[j]:
-            i = i+1
-        else:
-            j = j+1
+            if ( i+iSkipPointer < len(list1) and list[i+iSkipPointer] < list2[j]):
+                i = i + iSkipPointer
+            else:
+                i = i+1
+        else: # list1 > list2
+            if ( j+jSkipPointer < len(list2) and list[j+jSkipPointer] < list1[i]):
+                j = j + jSkipPointer
+            else:
+                j = j+1
 
     return resultList
 
@@ -84,12 +96,30 @@ def merge(list1, list2):
 # union for OR for 2 arrays
 def union(list1, list2):
     resultList = []
+    """
+    Trying to make faster -> do later if got time
+    -> O(2n)
+    i = j = 0
+    while (i < len(list1) and j < len(list2)):
+        if list1[i] == list2[j]:
+            resultList.append(list1[i])
+            i = i + 1
+            j = j + 1
+        elif list1[i] < list2[j]:
+            resultList.append(list1[i])
+            i = i+1
+        else:
+            resultList.append(list2[j])
+            j = j+1
+    """
+
+    # O(2n) + O(nlgn)
     for eachDocID in list1:
-        if eachDocID not in resultList:
-            resultList.append(eachDocID)
+        if int(eachDocID) not in resultList:
+            resultList.append(int(eachDocID))
     for eachDocID in list2:
-        if eachDocID not in resultList:
-            resultList.append(eachDocID)
+        if int(eachDocID) not in resultList:
+            resultList.append(int(eachDocID))
 
     return resultList.sort()
 
